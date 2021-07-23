bc3a4dc6d973efc11197fc75ad316fce89d7c7f4
noobj <jingfu99@gmail.com>
2021-07-23 21:24:46 +0800

    two pointer

--- DP/392_Is_Subsequence.ts
+++ DP/392_Is_Subsequence.ts
@@ -1,5 +1,4 @@
 function isSubsequence(s: string, t: string): boolean {
-    // return twoPointerSearch(s, t, 0, s.length - 1);
     if (s == t) return true;
 
     let indexS = 0;
@@ -14,30 +13,9 @@ function isSubsequence(s: string, t: string): boolean {
     return false;
 }
 
-function twoPointerSearch(s: string, t: string, i: number, j: number): boolean {
-    if (i > j) return true;
-
-    if (i == j) {
-        return t.indexOf(s[i]) == -1 ? false : true;
-    }
-
-    const searchFromHead = t.indexOf(s[i]);
-    const searchFromRear = t.indexOf(s[j]);
-
-    if (searchFromHead == -1 || searchFromRear == -1 || searchFromRear < searchFromHead)
-        return false;
-
-    return twoPointerSearch(s, t.slice(searchFromHead + 1, searchFromRear), i + 1, j - 1);
-}
-
 function main() {
     const hrstart = process.hrtime();
-    console.log(
-        isSubsequence(
-            'axc',
-            'auuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuggggggggguuuuuuuuuuuuuuuuuuuuuuuuuuuhbgllllllllgggggggggggggggggllllllllllllllllllllllllldc'
-        )
-    );
+    console.log(isSubsequence('axc', 'auuuuuuuuuuuuuxc'));
     const hrend = process.hrtime(hrstart);
     console.log(`Took ${hrend[1] / 1000000} milliseconds.`);
 }

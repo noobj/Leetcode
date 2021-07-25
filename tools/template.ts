9a0d6e54724f34bd0eaac77c3efa551d7e752340
noobj <jingfu99@gmail.com>
2021-07-25 14:59:15 +0800

    add new tool turn_title_to_file

---
+++ tools/template.ts
@@ -0,0 +1,13 @@
+function dummyfunction() {
+};
+
+function main() {
+    const hrstart = process.hrtime();
+    console.log(dummyfunction());
+    const hrend = process.hrtime(hrstart);
+    console.log(`Took ${hrend[1] / 1000000} milliseconds.`);
+}
+
+main();
+
+export const fake = 123;
---
+++ tools/turn_title_to_file.ts
@@ -0,0 +1,28 @@
+#!/usr/bin/env node
+import yargs from 'yargs/yargs';
+import fs from 'fs';
+
+const argv = yargs(process.argv.slice(2))
+    .usage('Usage: $0 <titles> [options]')
+    .example('$0 1. two sum -c DP', '')
+    .demandOption(['_'])
+    .options({
+        e: { type: 'string', default: 'ts', alias: 'extend' },
+        c: { type: 'string', default: '.', alias: 'category' }
+    })
+    .help('h')
+    .alias('h', 'help')
+    .parseSync();
+
+if (argv._.length == 0) {
+    console.log('Need to enter the title of leetcode question');
+    process.exit(1);
+}
+
+const fileName = `${argv._.map((v) => v.toString().toLowerCase()).join('_')}.${argv.e}`;
+
+const writeStream = fs.createWriteStream(process.cwd() + `/${argv.c}/${fileName}`);
+const fileStream = fs.createReadStream(process.cwd() + '/tools/template.ts');
+fileStream.pipe(writeStream);
+
+console.log(`${argv.c}/${fileName} created, lets do this!`);

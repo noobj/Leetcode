0ccbc49c6f975561b4b833a79cd5d3667e183625
noobj <jingfu99@gmail.com>
2021-08-02 13:22:34 +0800

    couldnt focus very well

---
+++ recursion/234_palindrome_linked_list.ts
@@ -0,0 +1,52 @@
+import { ListNode, printList, turnNumberIntoList } from '../modules/linked_list.module';
+
+function isPalindrome(head: ListNode | null): boolean {
+    let fast = head,
+        slow = head;
+    while (fast != undefined || fast != null) {
+        fast = fast?.next?.next;
+        slow = slow.next;
+    }
+
+    slow = reverseList(slow);
+
+    while (slow != null) {
+        if (slow.val != head.val) return false;
+
+        slow = slow.next;
+        head = head.next;
+    }
+
+    return true;
+}
+
+function reverseList(head: ListNode | null): ListNode | null {
+    if (head == null) return null;
+
+    const pre = new ListNode(undefined, null);
+    helper(pre, head);
+    return pre.next;
+}
+
+function helper(pre: ListNode, head: ListNode): ListNode {
+    if (head.next == null) {
+        pre.next = head;
+        return head;
+    }
+
+    const nextNode = helper(pre, head.next);
+    nextNode.next = head;
+    head.next = null;
+    return head;
+}
+
+function main() {
+    const hrstart = process.hrtime();
+    console.log(isPalindrome(turnNumberIntoList([3, 2, 3, 2, 3])));
+    const hrend = process.hrtime(hrstart);
+    console.log(`Took ${hrend[1] / 1000000} milliseconds.`);
+}
+
+main();
+
+export const fake = 123;

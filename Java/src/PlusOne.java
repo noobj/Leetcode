30d8c1d2d4cc1db53a04d85b12f553a0611a77ec
noobj <jingfu99@gmail.com>
2023-05-22 07:14:13 -0700

    optimized plus one

--- Java/src/PlusOne.java
+++ Java/src/PlusOne.java
@@ -4,21 +4,16 @@ import java.util.List;
 
 class PlusOne66 {
     public int[] plusOne(int[] digits) {
-        List<Integer> stack = new ArrayList<>(Arrays.asList());
-        int carry = 1;
-
-        for (int i = digits.length - 1; i >= 0 ; i--) {
-            int tmp = digits[i] + carry;
-
-            if (tmp == 10) {
-                carry = 1;
-                tmp = 0;
-            } else carry = 0;
-            stack.add(0, tmp);
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] < 9) {
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
         }
 
-        if (carry == 1) stack.add(0, 1);
-
-        return stack.stream().filter(t -> t != null).mapToInt(t -> t).toArray();
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+        return digits;
     }
 }
